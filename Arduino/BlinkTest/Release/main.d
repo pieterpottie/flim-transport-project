3f5d1887dae6ceab8adcbb9172bb823733a0d38b
anderson.zhi.li <anderson.zhi.li@gmail.com>
2012-07-16 12:35:33 +0000

    sdfadsf

---
+++ Arduino/BlinkTest/Release/main.d
@@ -0,0 +1,26 @@
+main.d main.o: ../main.cpp ../../libCore/arduino/Arduino.h \
+ ../../libCore/arduino/binary.h ../../libCore/arduino/WCharacter.h \
+ ../../libCore/arduino/WString.h ../../libCore/arduino/HardwareSerial.h \
+ ../../libCore/arduino/Stream.h ../../libCore/arduino/Print.h \
+ ../../libCore/arduino/Printable.h ../../libCore/arduino/new.h \
+ ../../libCore/arduino/pins_arduino.h
+
+../../libCore/arduino/Arduino.h:
+
+../../libCore/arduino/binary.h:
+
+../../libCore/arduino/WCharacter.h:
+
+../../libCore/arduino/WString.h:
+
+../../libCore/arduino/HardwareSerial.h:
+
+../../libCore/arduino/Stream.h:
+
+../../libCore/arduino/Print.h:
+
+../../libCore/arduino/Printable.h:
+
+../../libCore/arduino/new.h:
+
+../../libCore/arduino/pins_arduino.h:
