a433cd6f11ff9dceecd620863b09135c2437fa90
anderson.zhi.li@gmail.com <anderson.zhi.li@gmail.com@d1503cd0-42ad-d2fd-ff13-57d6d99988f8>
2012-08-19 06:30:33 +0000

    new update

--- trunk/Arduino/ProjectController/Release/LiquidCrystal.d
+++ trunk/Arduino/ProjectController/Release/LiquidCrystal.d
@@ -1,31 +1,49 @@
-LiquidCrystal.d LiquidCrystal.o: ../LiquidCrystal.cpp ../LiquidCrystal.h \
- ../../libCore/arduino/Print.h ../../libCore/arduino/WString.h \
- ../../libCore/arduino/Printable.h ../../libCore/arduino/new.h \
- ../../libCore/arduino/Arduino.h ../../libCore/arduino/binary.h \
- ../../libCore/arduino/WCharacter.h \
- ../../libCore/arduino/HardwareSerial.h ../../libCore/arduino/Stream.h \
- ../../libCore/arduino/Print.h ../../libCore/arduino/pins_arduino.h
-
-../LiquidCrystal.h:
-
-../../libCore/arduino/Print.h:
-
-../../libCore/arduino/WString.h:
-
-../../libCore/arduino/Printable.h:
-
-../../libCore/arduino/new.h:
-
-../../libCore/arduino/Arduino.h:
-
-../../libCore/arduino/binary.h:
-
-../../libCore/arduino/WCharacter.h:
-
-../../libCore/arduino/HardwareSerial.h:
-
-../../libCore/arduino/Stream.h:
-
-../../libCore/arduino/Print.h:
-
-../../libCore/arduino/pins_arduino.h:
+008" {
+			OVERRIDE_VALUE 49 
+		}
+		MACRO "ReferencePoint" {
+			AFTER_MOTION Yes 
+		}
+	}
+	WORD_VALUE "G" "40" {
+		VARIABLE "4007"
+		MACRO "CutterCompOff"
+		MACRO "Cancel3dToolOffset"
+	}
+	WORD_VALUE "G" "41" {
+		VARIABLE "4007"
+		MACRO "CutterCompLeft"
+	}
+	WORD_VALUE "G" "42" {
+		VARIABLE "4007"
+		MACRO "CutterCompRight"
+	}
+	WORD_VALUE "G" "43" {
+		VARIABLE "4008"
+		MACRO "ToolLengthCompPos"
+	}
+	WORD_VALUE "G" "44" {
+		VARIABLE "4008"
+		MACRO "ToolLengthCompNeg"
+	}
+	WORD_VALUE "G" "49" {
+		VARIABLE "4008"
+		MACRO "ToolLengthCompOff"
+		MACRO "RtcpOff"
+	}
+	WORD_VALUE "G" "52" {
+		MACRO "AbsoluteShiftNum" {
+			OVERRIDE_VALUE 1 
+		}
+	}
+	WORD_VALUE "G" "53" {
+		MACRO "NullMacro"
+	}
+	WORD_VALUE "G" "54-59" {
+		VARIABLE "4014"
+		MACRO "WorkCoord"
+	}
+	WORD_VALUE "G" "60" {
+		MACRO "IgnoreMacro"
+	}
+	WORD
--- trunk/Arduino/ProjectController/Release/subdir.mk
+++ trunk/Arduino/ProjectController/Release/subdir.mk
@@ -1,45 +1,26 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
+r for the frames
+P 
+P 	- sketch of a drive system: file:///media/shared/Google%20Drive/UVic/Engineering/4C-2/Engr%20466/Notes/Sketches/Optic_Projection_fig_226.jpg
+P 
+P 		
+P 		caption: "Fig. 226. Moving Picture Mechanism with Outside Shutter, O S.
+P 		a b Sprocket wheels moving with uniform velocity.
+P 		c Intermittent sprocket wheel which jerks down the short section of film between L and M.
+P 		i Idlers to hold the film on the sprocket wheels.
+P 		F Upper film reel, unwinding.
+P 		G Lower film reel, winding up.
+P 		S Aperture plate."
 
-# Add inputs and outputs from these tool invocations to the build variables 
-CPP_SRCS += \
-../AccelStepper.cpp \
-../DCMotor.cpp \
-../LCD.cpp \
-../LiquidCrystal.cpp \
-../PDController.cpp \
-../SoftPWM.cpp \
-../StepperMotor.cpp \
-../main.cpp 
-
-OBJS += \
-./AccelStepper.o \
-./DCMotor.o \
-./LCD.o \
-./LiquidCrystal.o \
-./PDController.o \
-./SoftPWM.o \
-./StepperMotor.o \
-./main.o 
-
-CPP_DEPS += \
-./AccelStepper.d \
-./DCMotor.d \
-./LCD.d \
-./LiquidCrystal.d \
-./PDController.d \
-./SoftPWM.d \
-./StepperMotor.d \
-./main.d 
-
-
-# Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.cpp
-	@echo 'Building file: $<'
-	@echo 'Invoking: AVR C++ Compiler'
-	avr-g++ -I../../libCore/arduino -Wall -Os -fpack-struct -fshort-enums -funsigned-char -funsigned-bitfields -fno-exceptions -mmcu=atmega328p -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-
+ID10006
+E 1055047682
+Dr1337276665
+Dh1337276685
+P Design Specs From Andrew
+P 
+P Required Design Components
+P --Transport mechanism-- to accurately advance the frames
+P         --i.e. geneva mechanism, or stepper motor with position feedback sensors..
+P         --film movement is to be synchronized with the capture function of a camera, taking images when a new frame is at rest
+P         --speed of advancing frame rate will be limited by the camera which is expected to only be capable of 1-3 fps
+P         --specifying and fabrication of necessary parts
+P --Film gate-- to align and flatten the
